642f3e7338f53a9085ba84968f15c22e3c0f9c3d
Joychao <joy@joychao.cc>
2013-04-12 21:56:10 +0800

    change dirs

--- DolrPHP3/DolrPHP/Dao/DefaultLogger.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Db_DefAultLogger implements Db_Logger
-{
-    public function log($string, $type = 1) {
-        if (!$type)
-            error_log($string);
-    }
-}
--- DolrPHP3/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('APP_NAME','Home');
-define('APP_PATH','./Home/');
-include './DolrPHP/DolrPHP.php';
