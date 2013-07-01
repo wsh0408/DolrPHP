c892a94921feb736669d3a196b511c5b63ae7e32
Joychao <joychao@live.com>
2013-07-01 17:50:18 +0800

    Update Trace.php
    樣式修改

--- DolrPHP/Template/Trace.php
+++ DolrPHP/Template/Trace.php
@@ -1,8 +1,8 @@
 <div class="clearfix"></div>
        <div id="dolrTraceTool" style="display:block;" onclick="document.getElementById('DolrPHPtraceInfo').style.display='block';this.style.display='none';">TRACE</div>
        <div class="traceInfo" id="DolrPHPtraceInfo" style="display:none;">
-        <div class="head"><span class="hd left">TRACE:</span><span class="close" onclick="var x=document.getElementById('DolrPHPtraceInfo');x.style.display='none';document.getElementById('dolrTraceTool').style.display='block';">—</span></div>
-            <div class="content">
+        <div class="traceInfoHead"><span class="hd left">TRACE:</span><span class="close" onclick="var x=document.getElementById('DolrPHPtraceInfo');x.style.display='none';document.getElementById('dolrTraceTool').style.display='block';">—</span></div>
+            <div class="traceInfoContent">
             <p><span>运行用时:</span>[TIME_USAGE] 秒</p>
             <p><span>内存占用:</span>[MEM_USAGE]</p>
             <p><span>当前页面:</span>[CURRENT_FILE]</p>
@@ -19,23 +19,23 @@
         </div>
     <style>
     .clearfix{clear:both;height:0;width:100%;}
-    #dolrTraceTool{wdith:100px;position:fixed;bottom:0;right:0;background:#fff;color:#777;padding:5px 10px;font-weight:bold;font-size:16px;box-shadow:0 0 6px #555;}
+    #dolrTraceTool{wdith:100px;position:fixed;bottom:0;right:0;background:#fff;color:#777;padding:5px 10px;font-weight:bold;font-size:16px;box-shadow:0 0 6px #555;text-align: left;}
     .traceInfo{ position:fixed;left:20%;bottom:5%; clear:both; width:80%; text-align:left;margin:0px auto; margin-bottom:60px;border:1px solid #ccc; width:60%; -webkit-box-shadow: 0px 0px 10px #999 inset;box-shadow: 0px 0px 10px #999; word-break:break-all; background: #fff;font-size:12px;padding:20px; color:#000;z-index:9999999;overflow:hidden;}
     .traceInfo .red {color:red;}
     .traceInfo *{margin:0;padding:0;font-family:Consolas,Verdana,"Microsoft YaHei", Geneva, sans-serif;}
-    .traceInfo .head{ padding: 0 5px 10px 10px; border-bottom: 1px solid #c0c0c0;  }
-    .traceInfo .head .hd{font-size:27px;}
-    .traceInfo .head span.close{ position:absolute; right:20px;top:20px; cursor:pointer; padding:0 5px; text-align:center; overflow:hidden; color:#777;font-size:22px;}
-    .traceInfo .head span.close:hover{color: #444;}
-    .traceInfo .content { padding: 10px; color: #666; }
-    .traceInfo .content .classList,.traceInfo .content .infoList { border: 1px solid #ccc; padding: 5px; margin: 5px; overflow:auto; max-height:100px;}
-    .traceInfo .content p { line-height: 1.5em; }
-    .traceInfo .content p span { font-weight: bold; text-align: left; display: inline-block; word-spacing:2px; margin-right: 5px; }
-    .traceInfo .content p span.class{ font-weight:normal;}
+    .traceInfo .traceInfoHead{ padding: 0 5px 10px 10px; border-bottom: 1px solid #c0c0c0;  }
+    .traceInfo .traceInfoHead .hd{font-size:27px;}
+    .traceInfo .traceInfoHead span.close{ position:absolute; right:20px;top:20px; cursor:pointer; padding:0 5px; text-align:center; overflow:hidden; color:#777;font-size:22px;}
+    .traceInfo .traceInfoHead span.close:hover{color: #444;}
+    .traceInfo .traceInfoContent { padding: 10px; color: #666; }
+    .traceInfo .traceInfoContent .classList,.traceInfo .traceInfoContent .infoList { border: 1px solid #ccc; padding: 5px; margin: 5px; overflow:auto; max-height:100px;}
+    .traceInfo .traceInfoContent p { line-height: 1.5em; text-align: left;}
+    .traceInfo .traceInfoContent p span { font-weight: bold; text-align: left; display: inline-block; word-spacing:2px; margin-right: 5px; }
+    .traceInfo .traceInfoContent p span.class{ font-weight:normal;}
     .traceInfo ol{ margin-left:30px; color:#444;}
     .traceInfo ol li{line-height: 1.5em;}
     .traceInfo ul strong.red{ color:#444;}
-    .traceInfo ul li{list-style:none;padding:3px 0;line-height: 1.2em;}
+    .traceInfo ul li，.traceInfo ol li{list-style:none;padding:3px 0;line-height: 1.2em;float:none;text-align: left;}
     /* scrollbar */
     ::-webkit-scrollbar {width: 5px; height: 11px; border: none; background: #ddd !important;}
     ::-webkit-scrollbar-track-piece {border: none; position: absolute; padding: 0; box-shadow: none; background-color:#ddd; border-radius: 1px;}
